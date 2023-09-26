00cc889f1d091112326bc534570f32aeddbcfdf6
Naor Azulay <nazulay@habana.ai>
2023-09-26 22:00:48 +0300

    after field cleanup

--- src/main/java/ResourceClasses/Comment.java
+++ src/main/java/ResourceClasses/Comment.java
@@ -6,9 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 public class Comment {
     private int postId;
     private int id;
-    private String name;
     private String email;
-    private String body;
 
     public int getPostId() {
         return postId;
@@ -26,27 +24,11 @@ public class Comment {
         this.id = id;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     public String getEmail() {
         return email;
     }
 
     public void setEmail(String email) {
         this.email = email;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 }
--- src/main/java/ResourceClasses/Post.java
+++ src/main/java/ResourceClasses/Post.java
@@ -15,9 +15,6 @@ import java.util.stream.Collectors;
 public class Post {
     private int userId;
     private int id;
-    private String title;
-    private String body;
-
 
     public int getUserId() {
         return userId;
@@ -33,22 +30,6 @@ public class Post {
 
     public void setId(int id) {
         this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
     }
 
     public Collection<Comment> getComments() {
